31ec8494e723453435243bee3ddc4e823e41202d
gochidera <147996767+gochidera@users.noreply.github.com>
2023-11-15 17:23:01 +0100

    My fifth commit

---
+++ week-5/pracrice-6/target/debug/deps/pracrice_6.d
@@ -0,0 +1,5 @@
+C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-5\pracrice-6\target\debug\deps\pracrice_6.exe: src\main.rs
+
+C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-5\pracrice-6\target\debug\deps\pracrice_6.d: src\main.rs
+
+src\main.rs:
---
+++ week-5/pracrice-6/target/debug/pracrice-6.d
@@ -0,0 +1,1 @@
+C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-5\pracrice-6\target\debug\pracrice-6.exe: C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-5\pracrice-6\src\main.rs
