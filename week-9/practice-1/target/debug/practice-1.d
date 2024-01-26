fc38f0b25ceaa914a21def9677f6506267ec8af7
gochidera <147996767+gochidera@users.noreply.github.com>
2024-01-26 22:49:33 +0100

    Add files via upload

---
+++ week-9/practice-1/target/debug/practice-1.d
@@ -0,0 +1,1 @@
+C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-9\practice-1\target\debug\practice-1.exe: C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-9\practice-1\src\main.rs
---
+++ week-9/practice-2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-9\practice-2\target\debug\deps\practice_2.exe: src\main.rs
+
+C:\Users\glori\OneDrive\Documents\g.okechukwuCOS101\week-9\practice-2\target\debug\deps\practice_2.d: src\main.rs
+
+src\main.rs:
